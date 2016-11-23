e2404febd412f5febaaf158c4f94e2bb7bd335ee
yejinbiao <651523696@qq.com>
2016-11-23 18:04:16 +0800

    leetcode

---
+++ src/excel_sheet_column_title/Solution.java
@@ -0,0 +1,12 @@
+package excel_sheet_column_title;
+
+/**
+ * 2016/11/23
+ */
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public String convertToTitle(int n) {
+    }
+}
---
+++ src/sqrt/Solution.java
@@ -0,0 +1,37 @@
+package sqrt;
+
+import java.util.Arrays;
+
+/**
+ * 2016/11/23
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int result = mySqrt(3);
+        System.out.println(result);
+
+    } public static int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        int low = 1;
+        int high = x;
+        while (low<high){
+            int mid = (low+high)>>1;
+            int result = x/mid;
+
+            if(result > mid){
+                low = mid;
+            }else if(result < mid){
+                high = mid;
+            }else {
+                return mid;
+            }
+            if(high - low ==1){
+                break;
+            }
+        }
+        return low;
+    }
+
+}
