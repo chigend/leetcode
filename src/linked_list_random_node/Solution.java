df9936c55731eafcb3d464c3bcd463755d4bfd6a
yejinbiao <651523696@qq.com>
2017-03-07 12:34:56 +0800

    linked list random node

---
+++ src/linked_list_random_node/Solution.java
@@ -0,0 +1,50 @@
+package linked_list_random_node;
+
+import model.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author yejinbiao
+ * @create 2017-03-06-下午5:42
+ */
+
+public class Solution {
+    private List<Integer> values = new ArrayList<>();
+    private Random random;
+
+    /**
+     * @param head The linked list's head.
+     *             Note that the head is guaranteed to be not null, so it contains at least one node.
+     */
+    public Solution(ListNode head) {
+        while (head != null) {
+            values.add(head.val);
+            head = head.next;
+        }
+        random = new Random();
+    }
+
+    /**
+     * Returns a random node's value.
+     */
+    public int getRandom() {
+        int randomNumber = random.nextInt(values.size());
+        return values.get(randomNumber);
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[10];
+        for (int count = 0; count < 100000; count++) {
+            int value = random.nextInt(10);
+            array[value]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(i + ":" + array[i]);
+        }
+
+    }
+}
