f7bb600156b2576dc2e363432b207cf7f9bfbb2c
yejinbiao <651523696@qq.com>
2017-03-11 19:28:45 +0800

    binary tree preorder

---
+++ src/binary_tree_preorder_traversal/Solution.java
@@ -0,0 +1,42 @@
+package binary_tree_preorder_traversal;
+
+import model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author yejinbiao
+ * @create 2017-03-11-下午3:36
+ */
+
+public class Solution {
+    public static void main(String [] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        t1.right = t2;
+        t2.left = t3;
+        List<Integer> result = preorderTraversal(t1);
+        System.out.println(result);
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                result.add(root.val);
+                stack.push(root);
+                root = root.left;
+            }
+            if (!stack.isEmpty()) {
+                root = stack.pop();
+                root = root.right;
+            }
+        }
+        return result;
+    }
+}
