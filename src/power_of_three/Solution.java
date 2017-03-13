c46b0c3921b856b161d43451d2d27be3dc796759
yejinbiao <651523696@qq.com>
2017-03-13 15:43:05 +0800

    power of three

---
+++ src/power_of_three/Solution.java
@@ -0,0 +1,20 @@
+package power_of_three;
+
+/**
+ * @author yejinbiao
+ * @create 2017-03-13-下午3:04
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println(i + ":" + isPowerOfThree(i));
+//        }
+        System.out.print(isPowerOfThree(243));
+    }
+
+    public static boolean isPowerOfThree(int n) {
+        double power = Math.log1p((double) n) / Math.log(3);
+        return Math.pow(3,(int)power) == n;
+    }
+}
