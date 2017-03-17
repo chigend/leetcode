85d37c5331d105071309aa2ab7acf98e74ca7c57
yejinbiao <651523696@qq.com>
2017-03-17 11:15:50 +0800

    binary tree level order traversal II

---
+++ src/binary_tree_level_order_traversal_II/Solution2.java
@@ -0,0 +1,51 @@
+package binary_tree_level_order_traversal_II;
+
+import model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yejinbiao
+ * @create 2017-03-17-上午11:01
+ */
+
+public class Solution2 {
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        node.left = node2;
+        node.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+        List<List<Integer>> result = levelOrderBottom(node);
+        System.out.print(result);
+    }
+
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        ArrayList<TreeNode> nodes = new ArrayList<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()) {
+            int size = nodes.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = nodes.remove(0);
+                list.add(node.val);
+                if (node.left != null) {
+                    nodes.add(node.left);
+                }
+                if (node.right != null) {
+                    nodes.add(node.right);
+                }
+            }
+            result.add(0,list);
+
+        }
+        return result;
+    }
+}
