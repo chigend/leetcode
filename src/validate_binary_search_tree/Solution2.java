a1d0eeec361f02e94c276cac091aab87ea15924c
yejinbiao <651523696@qq.com>
2017-03-22 21:00:36 +0800

    validate binary search tree

---
+++ src/validate_binary_search_tree/Solution2.java
@@ -0,0 +1,34 @@
+package validate_binary_search_tree;
+
+import model.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * iterative solution
+ */
+
+public class Solution2 {
+    public static void main(String[] args) {
+
+    }
+    public  boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> nodes = new Stack<>();
+        Integer previous = null;
+        while (root != null || !nodes.isEmpty()) {
+            while (root != null) {
+                nodes.push(root);
+                root = root.left;
+            }
+            if (!nodes.isEmpty()) {
+                TreeNode node = nodes.pop();
+                if (previous != null && node.val <= previous) {
+                    return false;
+                }
+                previous = node.val;
+                root = node.right;
+            }
+        }
+        return true;
+    }
+}
