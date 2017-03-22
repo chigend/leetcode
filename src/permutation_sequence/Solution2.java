b4cdb89108c7c05b6b7abdb90744d6fa120c8af8
yejinbiao <651523696@qq.com>
2017-03-22 17:28:27 +0800

    permutation sequence

--- src/permutation_sequence/Solution2.java
+++ src/permutation_sequence/Solution2.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Solution2 {
     public static void main(String[] args) {
-        String permutation = getPermutation(4,23);
+        String permutation = getPermutation(4,24);
         System.out.print(permutation);
     }
     public static String getPermutation(int n, int k) {
@@ -18,13 +18,13 @@ public class Solution2 {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i <= n; i++) numbers.add(i);
         StringBuilder sb = new StringBuilder();
+        k--;
         while (--n > 0) {
             int ways = factorials[n];
-            int quotient = k / ways;
+            int position = k / ways;
             int mod = k % ways;
-            int position = mod == 0 ? quotient  : quotient + 1;
-            sb.append(numbers.remove(position - 1));
-            k = mod == 0 ? ways : mod;
+            sb.append(numbers.remove(position));
+            k = mod;
         }
         sb.append(numbers.get(0));
         return sb.toString();
