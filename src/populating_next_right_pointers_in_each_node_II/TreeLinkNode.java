5e992bf3b2c16462d08a3fdab4baa2023445ae14
yejinbiao <651523696@qq.com>
2017-03-24 13:14:46 +0800

    populating next right pointers in each node II

---
+++ src/populating_next_right_pointers_in_each_node_II/TreeLinkNode.java
@@ -0,0 +1,15 @@
+package populating_next_right_pointers_in_each_node_II;
+
+/**
+ * @author yejinbiao
+ * @create 2017-03-24-上午10:35
+ */
+
+
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right,next;
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
