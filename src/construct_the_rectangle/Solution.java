35c67c50309e5bfd3c026b37e16a69dd755cab97
yejinbiao <651523696@qq.com>
2017-03-24 17:38:16 +0800

    construct the rectangle

--- src/construct_the_rectangle/Solution.java
+++ src/construct_the_rectangle/Solution.java
@@ -13,6 +13,26 @@ public class Solution {
         System.out.print(Arrays.toString(arr));
     }
 
+    /**
+     *
+     if(area == 0){
+     return result;
+     }
+     int length = (int)(Math.ceil(Math.sqrt(area)));
+     while(area%length != 0){
+     length++;
+     }
+     int width = area/length;
+     result[0] = length;
+     result[1] = width;
+     return result;
+     *  why increasing the length costs 90ms ,but decreasing the width just costs 5ms
+     *
+     *  think the area is a very large prime number ,e.g. 1111111111,the width should be 1
+     *  and length should be 1111111111, so increase length from sqrt(area) will cost more
+     *  than decrease width from sqrt(area)
+     */
+    //todo
     public static int[] constructRectangle(int area) {
         int[] result = new int[2];
         if(area == 0){
