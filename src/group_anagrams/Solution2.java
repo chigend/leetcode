4a889778d4126d131e7e8485ce42cea76af62ed9
yejinbiao <651523696@qq.com>
2017-03-27 15:27:20 +0800

    group anagrams

---
+++ src/group_anagrams/Solution2.java
@@ -0,0 +1,35 @@
+package group_anagrams;
+
+import java.util.*;
+
+/**
+ * @author yejinbiao
+ * @create 2017-03-27-上午11:07
+ */
+
+public class Solution2 {
+    public static void main(String[] args) {
+        String[] s = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.print(groupAnagrams(s));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<List<String>>();
+        Map<String, List<String>> table = new HashMap<>();
+
+        for (String str : strs) {
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            String s = new String(ch);
+            if (table.containsKey(s)) {
+                table.get(s).add(str);
+            } else {
+                List<String> group = new ArrayList();
+                group.add(str);
+                table.put(s, group);
+            }
+        }
+        for (List<String> g : table.values()) res.add(g);
+        return res;
+    }
+}
