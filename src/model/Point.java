d85d5b3bf11d0ac3b64ea9a1e6a7794ebfb5e1da
chigend <chigend@gmail.com>
2017-05-25 13:17:49 +0800

    model Point

---
+++ src/model/Point.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * @author yejinbiao
+ * @create 2017-05-25-10:29
+ */
+
+public class Point {
+    public int x;
+    public int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
