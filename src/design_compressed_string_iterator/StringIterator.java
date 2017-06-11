64590d0c4292a4885072f8dde2cce041e8b9a70d
chigend <chigend@gmail.com>
2017-06-11 19:44:50 +0800

    design compressed string iterator

--- src/design_compressed_string_iterator/StringIterator.java
+++ src/design_compressed_string_iterator/StringIterator.java
@@ -7,65 +7,54 @@ package design_compressed_string_iterator;
 public class StringIterator {
     private char current;
     private int count;
-    private char nextChar;
-    private int nextIndex = -1;
+
+    private int nextIndex ;
     private String compressedString;
 
     public StringIterator(String compressedString) {
-        if (compressedString == null || compressedString.length() == 0){
-            nextChar = ' ';
-        }else {
-            this.compressedString = compressedString;
-            current = compressedString.charAt(0);
-            boolean reachEnd = true;
-            for (int i = 1; i < compressedString.length(); i++) {
-                char c = compressedString.charAt(i);
-                if (Character.isAlphabetic(c)){
-                    reachEnd = false;
-                    nextIndex = i;
-                    nextChar = c;
-                    break;
-                }
-            }
-            if(!reachEnd){
-                count = Integer.parseInt(compressedString.substring(1,nextIndex));
-            }else {
-                nextChar = ' ';
-                count = Integer.parseInt(compressedString.substring(1,compressedString.length()));
-            }
-
+        if (compressedString == null || compressedString.length() == 0) {
+            nextIndex = -1;
         }
+        this.compressedString = compressedString;
     }
 
     public char next() {
-       if (count != 0) {
-           count --;
-           return current;
-       }
-       if (nextChar == ' ') return nextChar;
-       current = nextChar;
+        //if the current char has left,just return the current char,and count down the remain number
+        if (count != 0) {
+            count--;
+            return current;
+        }
+        //if the current char has been consumed out,and there is not another char ,return blank ' '
+        if (nextIndex == -1)
+            return ' ';
+        //update the current char since there is another char(index will not be -1)
+        current = compressedString.charAt(nextIndex);
         boolean reachEnd = true;
+        //the start index where the positive number starts
         int start = nextIndex + 1;
-        for (int i = nextIndex+1; i < compressedString.length(); i++) {
+        for (int i = start; i < compressedString.length(); i++) {
             char c = compressedString.charAt(i);
-            if (Character.isAlphabetic(c)){
+            if (Character.isAlphabetic(c)) {
+                //if find a another char,it means has not got the end of the string
                 reachEnd = false;
+                //and update the next char index
                 nextIndex = i;
-                nextChar = c;
                 break;
             }
         }
-        if(!reachEnd){
-            count = Integer.parseInt(compressedString.substring(start,nextIndex));
-        }else {
-            nextChar = ' ';
-            count = Integer.parseInt(compressedString.substring(start,compressedString.length()));
+        if (!reachEnd) {
+            count = Integer.parseInt(compressedString.substring(start, nextIndex));
+        } else {
+            //if reach the end of string,there is not another char exist,so set index to -1
+            nextIndex = -1;
+
+            count = Integer.parseInt(compressedString.substring(start, compressedString.length()));
         }
         count--;
         return current;
     }
 
     public boolean hasNext() {
-        return count != 0 || nextChar != ' ';
+        return count != 0 || nextIndex != -1;
     }
 }
