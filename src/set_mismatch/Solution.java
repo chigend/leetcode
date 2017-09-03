a931fe20c265aef148a334fda4b5b88cceebc042
chigend <chigend@gmail.com>
2017-09-03 18:34:02 +0800

    set mismatch

---
+++ src/set_mismatch/Solution.java
@@ -0,0 +1,33 @@
+package set_mismatch;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] result = findErrorNums(new int[]{1,2,2,4});
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] findErrorNums(int[] nums) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num < 0) {
+                num = -num;
+            }
+            if (nums[num - 1] < 0){
+                result[0] = num;
+                continue;
+            }
+            nums[num - 1] = -nums[num - 1];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result[1] = i + 1;
+                return result;
+            }
+        }
+        return result;
+    }
+}
