23fb3f3fdb5a69ce297e5eb9825095d5d33e2faf
chigend <chigend@gmail.com>
2017-09-06 17:45:11 +0800

    shortest_unsorted_continuous_subarray

---
+++ src/shortest_unsorted_continuous_subarray/Solution2.java
@@ -0,0 +1,28 @@
+package shortest_unsorted_continuous_subarray;
+
+import java.util.Arrays;
+
+public class Solution2 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int findUnsortedSubarray(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int end = -2;//init with 2,in case the array is already sorted
+        int start = -1;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(nums[i], max);
+            min = Math.min(nums[nums.length - 1 - i], min);
+            if (nums[i] < max) {
+                end = i;
+            }
+            if (nums[nums.length - 1 - i] > min) {
+                start = nums.length - 1 - i;
+            }
+        }
+        return end - start + 1;
+    }
+}
