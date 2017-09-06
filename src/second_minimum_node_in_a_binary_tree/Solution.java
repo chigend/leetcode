e6e8daeb531d796ffff8458c2e84b8db3ca01571
chigend <chigend@gmail.com>
2017-09-06 16:18:56 +0800

    second_minimum_node_in_a_binary_tree

---
+++ src/second_minimum_node_in_a_binary_tree/Solution.java
@@ -0,0 +1,34 @@
+package second_minimum_node_in_a_binary_tree;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import model.TreeNode;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public int findSecondMinimumValue(TreeNode root) {
+        Set<Integer> res = new HashSet<>();
+        traversal(res, root);
+        List<Integer> list = new ArrayList<>(res);
+        return res.size() < 2 ? -1 : list.get(1);
+    }
+    private void traversal(Set<Integer> res,TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        traversal(res, root.left);
+        traversal(res, root.right);
+    }
+}
