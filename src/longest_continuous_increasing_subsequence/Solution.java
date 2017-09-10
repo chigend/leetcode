88585ca02fcce24aca1f8e95d4b7278f0864d0be
chigend <chigend@gmail.com>
2017-09-10 19:09:53 +0800

    longest continuous increasing subsequence

---
+++ src/longest_continuous_increasing_subsequence/Solution.java
@@ -0,0 +1,28 @@
+package longest_continuous_increasing_subsequence;
+
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        int max = findLengthOfLCIS(new int[]{1,3,5,4,7});
+        System.out.println(max);
+    }
+
+    public static int findLengthOfLCIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int max = 1;
+        int countLen = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                countLen++;
+                max = Math.max(max, countLen);
+            }else {
+                countLen = 1;
+            }
+        }
+        return max;
+    }
+}
