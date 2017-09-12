f646f86ea542f4255cd2cfc168d9dda9ca42cc3a
chigend <chigend@gmail.com>
2017-09-12 12:50:17 +0800

    peeking iterator

---
+++ src/peeking_iterator/Main.java
@@ -0,0 +1,10 @@
+package peeking_iterator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/peeking_iterator/PeekingIterator.java
@@ -0,0 +1,45 @@
+package peeking_iterator;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class PeekingIterator implements Iterator<Integer> {
+    private Integer next;
+    private boolean peekEleConsumed;
+    private Iterator<Integer> iterator;
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        this.iterator = iterator;
+        peekEleConsumed = true;
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        if (next != null) {
+            return next;
+        }
+        next = iterator.next();
+        peekEleConsumed = false;
+        return next;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        if (peekEleConsumed) {
+            return iterator.next();
+        }else {
+            Integer result = next;
+            peekEleConsumed = true;
+            next = null;
+            return result;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !peekEleConsumed || iterator.hasNext();
+    }
+}
