dc948047d984f2a3a1ec8ab3dec31e5c782eb969
chigend <chigend@gmail.com>
2017-10-11 14:45:38 +0800

    employee importance

---
+++ src/employee_importance/Solution.java
@@ -0,0 +1,44 @@
+package employee_importance;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public int getImportance(List<Employee> employees, int id) {
+        Map<Integer, Employee> employeeMap = new HashMap<>();
+        for (Employee e : employees) {
+            employeeMap.put(e.id, e);
+        }
+        List<Integer> queue = new LinkedList<>();
+        queue.add(id);
+        int importance = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Employee e = employeeMap.get(queue.remove(0));
+                importance += e.importance;
+                queue.addAll(e.subordinates);
+            }
+        }
+        return importance;
+    }
+
+    class Employee {
+        // It's the unique id of each node;
+        // unique id of this employee
+        public int id;
+        // the importance value of this employee
+        public int importance;
+        // the id of direct subordinates
+        public List<Integer> subordinates;
+    }
+
+    ;
+}
