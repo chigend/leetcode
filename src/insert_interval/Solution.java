5ae4e5505c4604cf7a619346255b30c7ea1b4278
chigend <chigend@gmail.com>
2018-11-07 14:04:00 +0800

    insert interval

--- src/insert_interval/Solution.java
+++ src/insert_interval/Solution.java
@@ -12,43 +12,29 @@ public class Solution {
         System.out.println(insert(list, new Interval(6, 7)));
     }
 
-    public static List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         List<Interval> result = new ArrayList<>();
-        if (intervals.size() == 0) {
-            result.add(newInterval);
-            return result;
-        }
 
-        if (newInterval.start > intervals.get(intervals.size() - 1).end) {
-            intervals.add(newInterval);
-            return intervals;
-        }
-        int start = -1;
-        int end = -1;
-        int left = -1;
-        for (int i = 0; i < intervals.size(); i++) {
+        int left = 0;
+        for(int i = 0; i < intervals.size(); i++, left++) {
             Interval cur = intervals.get(i);
             if (newInterval.start > cur.end) {
                 result.add(cur);
-            } else {
-                start = Math.min(newInterval.start, cur.start);
-                while (i < intervals.size() && newInterval.end > intervals.get(i).end) {
-                    i++;
-                }
-                if (i == intervals.size()) {
-                    result.add(new Interval(start, newInterval.end));
-                }else if (newInterval.end < intervals.get(i).start) {
-                    result.add(new Interval(start, newInterval.end));
-                    result.add(intervals.get(i));
-                } else {
-                    result.add(new Interval(start, intervals.get(i).end));
-                }
-                left = ++i;
+            }else if(newInterval.end < cur.start) {
+                result.add(newInterval);
+                newInterval = null;
                 break;
+            }else{
+                newInterval.start = Math.min(cur.start, newInterval.start);
+                newInterval.end = Math.max(cur.end, newInterval.end);
             }
         }
-        for (int i = left; i < intervals.size(); i++) {
+
+        for(int i = left; i < intervals.size(); i++) {
             result.add(intervals.get(i));
+        }
+        if (newInterval != null) {
+            result.add(newInterval);
         }
         return result;
     }
