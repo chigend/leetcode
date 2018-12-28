c9d1225456792f8dd8694e47c8062669ac85a2b1
chigend <chigend@gmail.com>
2018-12-28 11:40:15 +0800

    model

--- src/model/Node.java
+++ src/model/Node.java
@@ -1,12 +1,21 @@
 package model;
 
+import java.util.List;
+
 public class Node {
     public int val;
     public Node prev;
     public Node next;
     public Node child;
 
+    public List<Node> children;
+
     public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
 
     public Node(int val) {
         this.val = val;
