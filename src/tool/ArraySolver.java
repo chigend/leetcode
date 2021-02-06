e5bcc5dc5da6901514437cd43c63c959f88ecb71
chigend <chigend@gmail.com>
2021-02-06 11:29:19 +0800

    tool

--- src/tool/ArraySolver.java
+++ src/tool/ArraySolver.java
@@ -2,7 +2,7 @@ package tool;
 
 public class ArraySolver {
     public static void main(String[] args) {
-        String array = "[[9,10],[5,8],[2,6],[1,5],[3,8],[4,9],[8,10],[4,10],[6,8],[7,9]]";
+        String array = "[[1,1],[2,2],[3,3]]";
         array = array.replaceAll("\\[","{");
         array = array.replaceAll("]","}");
         System.out.println(array);
