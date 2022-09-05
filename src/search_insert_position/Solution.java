bc4dbde4427b34b60e2b52507999a31d94755fa7
ye.jinbiao <ye.jinbiao@darcytech.com>
2022-09-05 18:07:37 +0800

    search insert position

--- src/search_insert_position/Solution.java
+++ src/search_insert_position/Solution.java
@@ -11,18 +11,16 @@ public class Solution {
     }
 
     public int searchInsert(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        while (low <= high) {
-            int mid = (low + high) >> 1;
-            if (nums[mid] > target) {
-                high = mid - 1;
-            }else if (nums[mid] < target) {
-                low = mid + 1;
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int mid = r + (l - r >> 1);
+            if (nums[mid] >= target) {
+                r = mid;
             }else {
-                return mid;
+                l = mid + 1;
             }
         }
-        return low;
+        return nums[r] >= target ? r : r + 1;
     }
 }
