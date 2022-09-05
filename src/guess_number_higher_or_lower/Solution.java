c7c80f83c6783630a9f54132ec70ce94c217f241
ye.jinbiao <ye.jinbiao@darcytech.com>
2022-09-05 13:07:40 +0800

    guess number higher or lower

--- src/guess_number_higher_or_lower/Solution.java
+++ src/guess_number_higher_or_lower/Solution.java
@@ -5,31 +5,27 @@ import java.util.Random;
 public class Solution {
 	public static int target = 1;
 	public static void main(String[] args) {
-			Random r = new Random();
-			int n = 1;
-//			target = r.nextInt(n) + 1;
-			int result = guessNumber(n);
-			System.out.println(result);
+		Solution solution = new Solution();
+		Random r = new Random();
+
+		target = r.nextInt(100) + 1;
+		System.out.println("目标数字:"+target);
+		System.out.println(solution.guessNumber(target));
 	}
 
-	public static int guessNumber(int n) {
-		long start  = 1;
-		long end = n;
-		int guessNumber = 0;
-		while(start <= end){
-			guessNumber =(int) ((start+end)>>1);
-			int response = guess(guessNumber);
-			if(response == -1){
-				end = guessNumber - 1;
-				continue;
-			}else if(response == 1){
-				start = guessNumber + 1;
-				continue;
-			}else{
-				return guessNumber;
+	public  int guessNumber(int n) {
+		int l  = 1;
+		int r = n;
+		while(l < r){
+			int mid = r + (l - r >> 1);
+			int res = guess(mid);
+			if(res == -1){
+				r = mid;
+			}else {
+				l = mid + 1;
 			}
 		}
-		return -1;
+		return guess(r) == 0 ? r: r - 1;
 	}
 	
 	public static int guess(int guess){
