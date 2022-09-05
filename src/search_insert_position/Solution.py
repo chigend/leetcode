2c540ea721c0876b0589fedf7dac90eb8e546beb
ye.jinbiao <ye.jinbiao@darcytech.com>
2022-09-05 18:04:53 +0800

    search insert position

---
+++ src/search_insert_position/Solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            mid = l + r >> 1
+            if nums[mid] >= target:
+                r = mid
+            else: l = mid + 1
+        return r if nums[r] >= target else r + 1
+
+
+if __name__ == '__main__':
+    s = Solution()
+    res = [0]
+    print(s.searchInsert(res, 1))
